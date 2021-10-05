523edf391c07c4f668391522c0503ba4755da382
Daniel Redondo <daniel28.redondo@gmail.com>
2021-10-05 13:22:40 +0200

    mi

--- provesc++/provesc++.cpp
+++ provesc++/provesc++.cpp
@@ -5,8 +5,38 @@ using namespace std;
 
 int main()
 {
-    
+    int c, g;
+
+
+    cout << "Introdueix el nombre inicial de conills:" << endl;
+    cin >> c;
+    while (x g = 0) {
+        cout "Error. Introdueix el nombre inicila de conills:" << endl;
+        cout << "Introdueix el nombre inicial de conills:" << endl;
+        cin >> c;
+    }
+
+    cout << "Introdueix el nombre inicial de guineus:" << endl;
+    cin >> g;
+    while (g <= 0) {
+        cout "Error. Introdueix el nombre inicila de conills:" << endl;
+        cout << "Introdueix el nombre inicial de conills:" << endl;
+        cin >> g;
+    }
+
 }
 
 
-a
+
+
+
+
+
+
+
+
+
+
+
+
+
